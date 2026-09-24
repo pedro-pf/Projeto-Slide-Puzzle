e991c22d34ab0929c8c8c15aa3815ffbd912700f
Erick Leonardus <erickleonardus123@gmail.com>
2026-09-24 08:41:29 -0300

    melhoria na iinterface

--- draw.c
+++ draw.c
@@ -4,70 +4,137 @@
 #include "board.c"
 #include <math.h>
 
-
 int main(void) {
+
     InitWindow(900, 500, "Jogo");
+
+    Texture2D mesa = LoadTexture("Assets/mesa.png");
+
+    Color cor = BLUE;
+    bool c = true;
+
     Board *tab = board_create(9);
     board_initialize(tab);
 
-     Color cor = BLUE;
-     bool c = true;
-     
-     //Tamanho do tabuleiro;
-     int tr = 350;
-     int lr = 350;
+    // Tamanho inicial do tabuleiro
+    int tr = 350;
+    int lr = 350;
 
-     //Posição do tabuleiro;
-     int x = (GetRenderWidth() - tr) /2; 
-     int y = (GetRenderHeight() - lr) /2;
+    while (!WindowShouldClose()) {
 
-     
+        // Tela cheia
+        if (IsKeyPressed(KEY_F11)) {
+            ToggleFullscreen();
+        }
 
-    while(!WindowShouldClose()) {
+        // =====================================================
+        // TAMANHO ATUAL DA ÁREA DE RENDERIZAÇÃO
+        // =====================================================
 
-    //Tela cheia (faltam configurações);
-    if (IsKeyPressed(KEY_F11)) {
-        ToggleFullscreen();
-    }
+        int largura_tela = GetRenderWidth();
+        int altura_tela = GetRenderHeight();
 
 
-    BeginDrawing();
+        // =====================================================
+        // MESA
+        // =====================================================
+
+        Rectangle Sf = {
+            0.0f,
+            0.0f,
+            (float)mesa.width,
+            (float)mesa.height
+        };
+
+       Rectangle Df = {
+            -230.0f,
+            -135.0f,
+            (float)GetRenderWidth() + 450,
+            (float)GetRenderHeight() + 300
+        };
 
 
-    ClearBackground(cor);
+        // Mantém o tabuleiro no centro da tela
+        int x = (largura_tela - tr) / 2;
+        int y = (altura_tela - lr) / 2;
 
-    //Desenhar tabuleiro
-    DrawRectangle(x, y, tr, lr, YELLOW);
+        int xr = x;
+        int yr = y;
 
-    
-    //Posição das peças;
-     int xr = x;
-     int yr = y;
+        int tamanho_lado = sqrt(tab->tamanho);
 
-     //Tamanho das peças;
-     int trr = tr/(sqrt(tab->tamanho));
-     int lrr = lr/(sqrt(tab->tamanho));
+        int trr = tr / tamanho_lado;
+        int lrr = lr / tamanho_lado;
 
-     //Centro das peças;
-     int ctp = trr/2;
-     int clp = lrr/2;
+        int ctp = trr / 2;
+        int clp = lrr / 2;
 
-     //Contador das peças;
-     int num = 0;
-     
-     //Desenhar cada peça do tabuleiro
-    for (int j = 0; j < sqrt(tab->tamanho); j++) {
-        xr = x;
-        for (int i = 0; i < sqrt(tab->tamanho); i++) {
-            if (num != 0) {
-            DrawRectangle(xr + 1, yr + 1, trr - 1, lrr - 1, PURPLE);
-            DrawText(TextFormat("%d", tab->tabuleiro[num]), xr + ctp, yr + clp, 2, WHITE);
+        int num = 0;
+
+
+        BeginDrawing();
+
+        ClearBackground(cor);
+
+        // Mesa ocupa toda a tela
+        DrawTexturePro(
+            mesa,
+            Sf,
+            Df,
+            (Vector2){0.0f, 0.0f},
+            0.0f,
+            WHITE
+        );
+
+
+        // Tabuleiro
+        DrawRectangle(
+            x,
+            y,
+            tr,
+            lr,
+            YELLOW
+        );
+
+
+        // Peças
+        for (int j = 0; j < tamanho_lado; j++) {
+
+            xr = x;
+
+            for (int i = 0; i < tamanho_lado; i++) {
+
+                if (num != 0) {
+
+                    DrawRectangle(
+                        xr + 1,
+                        yr + 1,
+                        trr - 1,
+                        lrr - 1,
+                        PURPLE
+                    );
+
+                    DrawText(
+                        TextFormat("%d", tab->tabuleiro[num]),
+                        xr + ctp,
+                        yr + clp,
+                        2,
+                        WHITE
+                    );
+                }
+
+                xr += trr;
+                num++;
             }
-            xr += trr;
-            num++;
+
+            yr += lrr;
+        }
+
+        EndDrawing();
     }
-        yr += lrr;
-}
-    EndDrawing();
-    }
+
+    UnloadTexture(mesa);
+    CloseWindow();
+
+    return 0;
 }
